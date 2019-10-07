6f14260a3645b037183ec37d0a03b1f4691305f9
Kang Yuyang <tsmagic@126.com>
2019-10-07 17:13:22 +0800

    54

---
+++ src/test/java/Tu4/kangyuyang/po.java
@@ -0,0 +1,19 @@
+package Tu4.kangyuyang;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class po {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
